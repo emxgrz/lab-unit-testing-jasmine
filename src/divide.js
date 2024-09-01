15ab61c02d33e4fcdcbf211be9b76d019e297733
Emma <emmamartinezgarcia2806@gmail.com>
2024-09-01 21:50:42 +0200

    ejercicio jasmine, creo que realizado correctamente

--- src/divide.js
+++ src/divide.js
@@ -1,3 +1,12 @@
-function divide () {
-       
+function divide(a, b) {
+
+    if (a === 0 || b === 0) {
+    return undefined;
+  } 
+  else if (typeof a !== "number" || typeof b !== "number") {
+    return undefined;
+  } 
+  else {
+    return a / b;
+  }
 }
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,28 @@
-// UNCOMMENT THE CODE BELOW TO START
-
-/*
-
-describe("Iteration 2", () => {
-    describe("Function - divide", () => {
+describe("Iteration 2: divide", () => {
         
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("should be defined", () => {
+    expect (divide).toBeDefined();
+});
 
-    })    
+    it ("should take two numbers as arguments", () => {
+        expect (divide.length).toBe(2);
+    });
+
+    it ("should return the devision of the two numbers", ()=> {
+        expect (divide (1,2)).toEqual(0.5);
+        expect (divide(3,4)).toEqual(0.75);
+        expect (divide (100,50)).toEqual(2);
+    });
+
+    it ("should return undefined if arguments are not provided", () => {
+        expect (divide(1)).toEqual(undefined);
+        expect (divide()).toEqual(undefined);
+        expect (divide(undefined,1)).toEqual(undefined);
+    });
+
+    it ("should return undefined if arguments are not a number", () => {
+        expect (divide(1,"2")).toEqual(undefined);
+        expect (divide("3", 4)).toEqual(undefined);
+        expect (divide("100", "47")).toEqual(undefined);
+})  
 })
-
-*/
